992f0e9ee867ec2f01477c6172ad351d58271308
Kevin <diasks2@gmail.com>
2013-02-13 14:50:11 +0900

    Add foundation styling

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,12 +5,12 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the h1 'FridgeChart'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'FridgeChart')
     end
 
     it "should have the title 'FridgeChart'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
                         :text => "FridgeChart | Home")
     end
