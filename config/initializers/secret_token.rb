dea4e57864efdebcd10269c11c20102a4fdb54af
Kevin <diasks2@gmail.com>
2013-02-13 14:05:04 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fridgechart::Application.config.secret_token = '8d2882bdca28a1ea7302e59296317b2859702dbb0d20d250724ba69728781a93ae589ea1e608e12a91b419d3981ad75d1c94ace1216a67f2a2c0c429f5785f3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fridgechart::Application.config.session_store :cookie_store, key: '_fridgechart_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fridgechart::Application.config.session_store :active_record_store
